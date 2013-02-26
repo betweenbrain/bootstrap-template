cbaac5051238e63928537920851615ddf33640af
Matt Thomas <matt@betweenbrain.com>
2013-02-26 16:27:04 -0500

    Adding basic grid example

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $doc->addStyleSheet('templates/'.$this->template.'/css/template.css');
 			<a class="brand" href="<?php echo $this->baseurl; ?>"><?php echo htmlspecialchars($this->params->get('sitedescription')) ?></a>
 			<?php if ($this->countModules('position-1')) : ?>
 			<div class="nav-collapse collapse">
-				<jdoc:include type="modules" name="position-1" style="none" />
+				<jdoc:include type="modules" name="position-1" />
 			</div>
 			<?php endif; ?>
 		</div>
@@ -49,8 +49,15 @@ $doc->addStyleSheet('templates/'.$this->template.'/css/template.css');
 </div>
 
 <div class="container">
-	<jdoc:include type="message" />
-	<jdoc:include type="component" />
+	<div class="span8">
+		<jdoc:include type="modules" name="position-3" />
+		<jdoc:include type="message" />
+		<jdoc:include type="component" />
+	</div>
+	<div class="span3">
+		<jdoc:include type="modules" name="position-7" style="xhtml" />
+	</div>
 </div>
+
 </body>
 </html>
