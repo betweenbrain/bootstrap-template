b7e10b309c81374f7a9412813a9fef1b1619ab4e
Matt Thomas <matt@betweenbrain.com>
2013-05-10 13:12:31 -0400

    Load core Bootstrap CSS and Bootstrap bugfixes using class loader method. See http://docs.joomla.org/JHtml::_/11.1

--- index.php
+++ index.php
@@ -15,9 +15,8 @@
 // http://docs.joomla.org/Javascript_Frameworks#Twitter_Bootstrap_Javascript_Framework
 JHtml::_('bootstrap.framework');
 
-// Load core Bootstrap CSS and Bootstrap bug fixes from /media/jui/css/ (bootstrap.min.css, bootstrap-responsive.min.css, bootstrap-extended.css)
-// https://github.com/joomla/joomla-cms/blob/master/libraries/cms/html/bootstrap.php#L611
-JHtmlBootstrap::loadCss(TRUE, $this->direction);
+// Load core Bootstrap CSS and Bootstrap bugfixes using class loader method. See http://docs.joomla.org/JHtml::_/11.1
+JHtml::_('bootstrap.loadCss');
 
 // Returns a reference to the application object
 $app = JFactory::getApplication();
